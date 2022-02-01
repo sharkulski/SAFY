02dd02536fe4408a012050f3d2de7ce768aa87ba
sharkulski <artsharkulskiy@gmail.com>
2022-02-01 19:54:32 +0100

    Implementing SQL Repository

--- SafyWebUI/App_Start/UnityConfig.cs
+++ SafyWebUI/App_Start/UnityConfig.cs
@@ -1,6 +1,7 @@
 using Safy.Core.Contracts;
 using Safy.Core.Models;
 using Safy.DataAccess.InMemory;
+using Safy.DataAccess.SQL;
 using System;
 
 using Unity;
@@ -45,8 +46,8 @@ namespace SafyWebUI
 
             // TODO: Register your type's mappings here.
             // container.RegisterType<IProductRepository, ProductRepository>();
-            container.RegisterType<IRepository<Product>, InMemoryRepository<Product>>();
-            container.RegisterType<IRepository<ProductCategory>, InMemoryRepository<ProductCategory>>();
+            container.RegisterType<IRepository<Product>, SQLRepository<Product>>();
+            container.RegisterType<IRepository<ProductCategory>, SQLRepository<ProductCategory>>();
         }
     }
 }
